093254e2906a15ceabc6e1d622ec4c92d70d1f07
sn0wpokas <91987319+sn0wpokas@users.noreply.github.com>
2021-10-05 16:37:32 -0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -149,10 +149,10 @@ def loop6():
 
 print("Author: https://github.com/NoNameoN-A")
 
-vidUrl = "https://www.tiktok.com/@github_nonameon/video/6898692248968400130" #Change it
+vidUrl = "https://www.tiktok.com/@vulgo_snowzin/video/6959251595120971013" #Change it
 
 bot = int(input("What do you want to do?\n1 - Auto views(500)\n2 - Auto hearts\n3 - Auto (FIRST) comments heart\n4 - Auto followers\n5 - Auto Share\n6 - Simple reload\n"))
-i = 0
+i = 3
 
 chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
